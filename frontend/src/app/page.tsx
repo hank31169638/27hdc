7ebbad4f179e493cf785a72880f19068e9aae357
hank <h31169638@gmail.com>
2024-12-27 03:24:19 +0800

    update animation

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -2,14 +2,57 @@
 
 import Section00 from "@/components/section/section00";
 import Section01 from "@/components/section/section01";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import ScrollTrigger from "gsap/ScrollTrigger";
+import { useRef } from "react";
 
+// 註冊 ScrollTrigger 插件
+gsap.registerPlugin(ScrollTrigger);
 
 export default function Home() {
+    const section0Ref = useRef(null);
+    const section1Ref = useRef(null);
+
+    useGSAP(() => {
+        gsap.set([section0Ref.current, section1Ref.current], {
+            opacity: 0,
+            y: 50
+        });
+
+        gsap.to(section0Ref.current, {
+            opacity: 1,
+            y: 0,
+            duration: 1,
+            ease: "power2.out",
+            scrollTrigger: {
+                trigger: section0Ref.current,
+                start: "top center",
+                toggleActions: "play none none reverse",
+            }
+        });
+
+        gsap.to(section1Ref.current, {
+            opacity: 1,
+            y: 0,
+            duration: 1,
+            ease: "power2.out",
+            scrollTrigger: {
+                trigger: section1Ref.current,
+                start: "top center",
+                toggleActions: "play none none reverse",
+            }
+        });
+    }, []);
 
     return (
         <div className="relative w-full flex flex-col justify-center items-center">
-            <Section00/>
-            <Section01/>
+            <div ref={section0Ref} className="w-full">
+                <Section00/>
+            </div>
+            <div ref={section1Ref}>
+                <Section01/>
+            </div>
         </div>
     );
 }
